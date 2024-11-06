295e07ca5a28df966329c3c43b6f715c1e4fdfea
mandeotv1234 <22120201@student.hcmus.edu.vn>
2024-11-06 11:34:45 +0700

    22120201- product detail

--- src/routes/sites.js
+++ src/routes/sites.js
@@ -3,15 +3,7 @@ const router =express.Router();
 
 const sitesController= require('../app/controllers/SitesController');
 
-
-
 router.get('/',sitesController.index); 
-
-
-//router.get('/', sitesController.viewProductDetails);
-
-
-//router.get('/',sitesController.welcome);
 router.get('/registration',sitesController.viewRegistration);
 //router.post('/login',sitesController.login);
 //router.post('/register',sitesController.register);
